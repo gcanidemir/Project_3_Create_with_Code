812c39328eda2fc6e87a61e5502fba7d6b321b71
Görkem Canıdemir <40427109+gcanidemir@users.noreply.github.com>
2025-05-28 21:02:53 +0300

    src uploaded

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30.0f;
+    private float leftBound = -15.0f;
+    private PlayerController playercontollerScript;
+
+    void Start()
+    {
+        playercontollerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playercontollerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
